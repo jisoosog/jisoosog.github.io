971d2384728768caa4fde78b95cdf5117cc4aff9
Bubacuba <danielaskling03@gmail.com>
2021-08-27 14:12:49 +0200

    yse

---
+++ Hemsida_Rep/Javascript.js
@@ -0,0 +1,20 @@
+// function ChangeStart(){
+//     var idet = document.getElementsByTagName("div")[0];
+//     document.getElementById("start").style.display = "block"
+//     document.getElementById("sbout").style.display = "none"
+//     document.getElementById("body-content").style.display = "none"
+// }
+
+// function ChangeAbout(){
+//     var idet = document.getElementsByTagName("div")[0];
+//     document.getElementById("start").style.display = "none"
+//     document.getElementById("about").style.display = "block"
+//     document.getElementById("body-content").style.display = "none"
+// }
+
+function ChangeSpecs(){
+    var idet = document.getElementsByTagName("div")[2];
+    document.getElementById("start").style.display = "none"
+    document.getElementById("about").style.display = "none"
+    document.getElementById("body-content").style.display = "block"
+}
